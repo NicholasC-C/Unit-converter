15361522170ffbe05fcbdaa412d73cf5db066e9d
Nicholas Christiansen <salomangook@gmail.com>
2022-04-28 12:11:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+using Unit_converter;
+
+float input_value;
+while (true)
+{
+    do
+    {
+        Clear();
+        WriteLine("--- Convert units ---");
+        WriteLine("Value you want to convert");
+    } while (!float.TryParse(ReadLine(), out input_value));
+
+    WriteLine("Which unit do you wanna convert from?");
+    WriteLine("1. m/s");
+    WriteLine("2. km/t");
+    WriteLine("3. mph");
+    WriteLine("4. knots");
+    string from_unit = ReadKey(true).KeyChar.ToString();
+    Clear();
+    WriteLine("Unit to convert to?");
+    if (from_unit != "1")
+        WriteLine("1. m/s");
+    if (from_unit != "2")
+        WriteLine("2. km/t");
+    if (from_unit != "3")
+        WriteLine("3. mph");
+    if (from_unit != "4")
+        WriteLine("4. knots");
+    string to_unit = ReadKey(true).KeyChar.ToString();
+    Clear();
+    WriteLine(convert_units(input_value, from_unit, to_unit));
+
+    ReadKey();
+}
+
+static string convert_units(float input_value, string from_unit, string to_unit)
+{
+    float result = 0;
+    string unit = "";
+    switch (from_unit)
+    {
+        // m/s
+        case "1":
+            if (to_unit == "1")
+                result = input_value;
+            else if (to_unit == "2")
+                result = ms.to_kmph(input_value);
+            else if (to_unit == "3")
+                result = ms.to_mph(input_value);
+            else if (to_unit == "3")
+                result = ms.to_knots(input_value);
+            unit = ms.name;
+            break;
+        // km/t
+        case "2":
+            if (to_unit == "1")
+                result = kmh.to_SI_unit(input_value);
+            else if (to_unit == "2")
+                result = input_value;
+            else if (to_unit == "3")
+                result = ms.to_mph(kmh.to_SI_unit(input_value));
+            else if (to_unit == "4")
+                result = ms.to_knots(kmh.to_SI_unit(input_value));
+            unit = kmh.name;
+            break;
+        // mph
+        case "3":
+            if (to_unit == "1")
+                result = mph.to_SI_unit(input_value);
+            else if (to_unit == "2")
+                result = ms.to_kmph(mph.to_SI_unit(input_value));
+            else if (to_unit == "3")
+                result = input_value;
+            else if (to_unit == "4")
+                result = ms.to_knots(mph.to_SI_unit(input_value));
+            unit = mph.name;
+            break;
+        // knots
+        case "4":
+            if (to_unit == "1")
+                result = knots.to_SI_unit(input_value);
+            else if (to_unit == "2")
+                result = ms.to_kmph(knots.to_SI_unit(input_value));
+            else if (to_unit == "3")
+                result = ms.to_mph(knots.to_SI_unit(input_value));
+            else if (to_unit == "4")
+                result = input_value;
+            unit = knots.name;
+            break;
+    }
+    return $"{result} {unit}";
+}
+
---
+++ Units.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_converter
+{
+    public static class ms
+    {
+        public static string name = "m/s";
+        public static float to_kmph(float speed)
+        {
+            return speed * 3.6f;
+        }
+        public static float to_mph(float speed)
+        {
+            return speed * 2.237f;
+        }
+        public static float to_knots(float speed)
+        {
+            return speed * 1.943844f;
+        }
+    }
+
+    public static class kmh
+    {
+        public static string name = "km/h";
+        public static float to_SI_unit(float speed)
+        {
+            return speed / 3.6f;
+        }
+    }
+    public static class mph
+    {
+        public static string name = "mph";
+        public static float to_SI_unit(float speed)
+        {
+            return speed / 2.237f;
+        }
+    }
+    public static class knots
+    {
+        public static string name = "knots";
+        public static float to_SI_unit(float speed)
+        {
+            return speed / 1.943844f;
+        }
+    }
+}
